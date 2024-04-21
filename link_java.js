b8631336ce264b2e99516fa6cb7ce4b1261ab029
Chase Joyner <chasej@clemson.edu>
2024-04-21 18:08:02 -0400

    Small changes to colors

--- link_java.js
+++ link_java.js
@@ -3,9 +3,12 @@ const allLinks = document.querySelectorAll('.dynamic-link');
 allLinks.forEach(link => {
   link.addEventListener('click', function() {
     // Remove id from all links
-    links.forEach(link => link.removeAttribute('id'));
+    links.forEach(link => {
+            link.removeAttribute('id');
+            link.style.color = '';
+    });
 
-    // Add 'underline' id to the clicked link
-    this.id = 'underline';
+    // Add decorations to link
+    this.id = 'active-link';
   });
 });
